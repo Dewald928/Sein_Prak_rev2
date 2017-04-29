8a0b9cf830e09adff0b09772b76c8f1572e0fcf5
Dewald928 <dewald123@rocketmail.com>
2017-04-29 22:54:42 +0200

    added dft and idft :yum:

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QImage>
+#include <complex> // om komplekse wiskunde te doen
+#include <QVector>
+#include <QList>
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -29,12 +33,33 @@ private slots:
 
     void on_btn_dft_clicked();
 
+    void on_btn_idft_clicked();
+
 private:
     Ui::MainWindow *ui;
 
-    QImage *img_in, *img_red, *img_green, *img_blue, *img_grey, *img_dft_real, *img_dft_ima, *img_idft;
+    QImage *img_in, *img_red, *img_green, *img_blue, *img_grey, *img_dft_amp, *img_dft_phase, *img_idft;
     QImage *img_sobel_total[3];
     QImage *img_sobel_recon;
+
+    //complex<double> *data; //complex datatype
+    //complex<double> *dataout; //complex datatype
+    QVector< complex < double > > complex_1D;
+    QVector< QVector < complex < double > > > complex_2D;
+    QVector< QVector < complex < double > > > inverse_2D;
+
+    //idft 1-D Function
+    void IDFT1();
+
+    //dft 1-D Function
+    void DFT1();
+
+//    //fft 1-D function
+//    complex<double> *fft1(complex<double> *data, unsigned int size, int log2n,  bool inverse);
+
+//    //fft 2-D function
+//    complex<double> **fft2(complex<double> **data, int r, int c, bool inverse);
+
 
     void sobel(QImage *img, int iteration);
 
@@ -51,6 +76,8 @@ private:
 
     int xKernel;
     int yKernel;
+
+    bool inverse = false;
 };
 
 #endif // MAINWINDOW_H
